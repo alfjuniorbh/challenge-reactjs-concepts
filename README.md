e5c1b25f62a844a2f05fee53658db68a93c83766
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-04-19 14:40:27 +0100

    ReactJS Concepts Challenge

---
+++ README.md
@@ -0,0 +1,16 @@
+<img alt="GoStack" src="src/assets/reactjs.png" />
+
+<h3 align="center">
+  ReactJS Concepts Challenge
+</h3>
+
+## :rocket: About the challenge
+
+During this project, a simple application was created with React JS and Axios to consume an API previously created in NodeJS and Express.
+A simple layout using CSS and React Icons was implemented to improve the visualization of the result.
+
+In addition, of course, to keep the tests running without interruption.
+
+Below is a preview of how the application looks.
+
+<img alt="GoStack" src="src/assets/reactjs.png" />
--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+import { IoIosList, IoMdTrash } from 'react-icons/io';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepository(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      url: `https://github.com/alfjuniorbh/${Date.now()}`,
+      title: `Desafio ReactJS ${Date.now()}`,
+      techs: ['NodeJS', 'ReactJS', 'ReactNative'],
+    });
+
+    setRepository([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const repos = repositories.filter((repository) => repository.id !== id);
+    setRepository(repos);
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div className='container'>
+      <h1>
+        <IoIosList />
+        Repository Lists
+      </h1>
+      <ul data-testid='repository-list'>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            <div className='divide'></div>
+            <p>
+              {repository.title} <span>{repository.url}</span>
+            </p>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button
+              data-testid='remove'
+              onClick={() => handleRemoveRepository(repository.id)}
+            >
+              <IoMdTrash />
+            </button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Add Repository</button>
     </div>
   );
 }
